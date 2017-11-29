e6164fc93c6eb6a31cf04951a044cf379dbfd0e9
artbohr <26352035+artbohr@users.noreply.github.com>
2017-11-30 00:45:44 +0100

    Format app.js

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ d3.json(URL).get((error,data)=>{
   } else {
 
   const y = d3.scaleLinear()
-              .domain(d3.extent(data.data, (d) => { return d[1]; }))
+              .domain(d3.extent(data.data, d => d[1]))
               .range([height, 0])
               .nice();
 
   const x = d3.scaleTime()
-                .domain(d3.extent(data.data, (d) => { return ( parseDate(d[0])); } ))
+                .domain(d3.extent(data.data, d => parseDate(d[0])))
                 .range([0, width]);
 
   const yAxis = d3.axisLeft(y);
@@ -32,22 +32,22 @@ d3.json(URL).get((error,data)=>{
             .data(data.data)
             .enter().append("rect")
                   .attr("fill",'#8F5973')
-                  .attr("height",(d)=>{ return height - y(d[1]); })
+                  .attr("height", d=> height - y(d[1]))
                   .attr("width",Math.ceil(width/data.data.length))
-                  .attr("x",(d,i)=>{ return x(parseDate(d[0])); })
-                  .attr("y",(d)=>{ return y(d[1]); })
-                  .on("mouseover", function() {
-                    d3.select(this).style("fill", "#BDC2C6");
-                    tooltip.style("visibility", "visible")
+                  .attr("x",(d,i)=> { return x(parseDate(d[0])); })
+                  .attr("y", d=> y(d[1]))
+                    .on("mouseover", function() {
+                      d3.select(this).style("fill", "#BDC2C6");
+                      tooltip.style("visibility", "visible")
+                      })
+                    .on("mouseout", function() {
+                      d3.select(this).style("fill", "#8F5973");
+                      tooltip.style("visibility", "hidden");
                     })
-                  .on("mouseout", function() {
-                    d3.select(this).style("fill", "#8F5973");
-                    tooltip.style("visibility", "hidden");
-                  })
-                  .on("mousemove", function(d) {
-                    tooltip.select("text").text(`GDP: $ ${d[1]} Billion
-                       in ${yearMonth(new Date (d[0]))}`);
-                  });
+                    .on("mousemove", function(d) {
+                      tooltip.select("text").text(`GDP: $ ${d[1]} Billion
+                         in ${yearMonth(new Date (d[0]))}`);
+                    });
 
   let tooltip = chartGroup.append("g").attr("class","tooltip").style("visibility", "hidden");
   tooltip.append("text").attr("x","50").attr("y","30").style("font-size","20px").attr("font-weight","bold");
